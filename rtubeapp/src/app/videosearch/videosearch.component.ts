5e19c7e3e0e20a409ab51b66297187576a453c8b
skillersdev <sridhar.skillers@gmail.com>
2020-04-30 20:38:45 +0530

    by mani

---
+++ rtubeapp/src/app/videosearch/videosearch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-videosearch',
+  templateUrl: './videosearch.component.html',
+  styleUrls: ['./videosearch.component.css']
+})
+export class VideosearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
